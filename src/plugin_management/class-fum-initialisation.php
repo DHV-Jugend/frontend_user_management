6b20e99516b40344c712859499dcb98e3d581840
Christoph Bessei <chris@bessei-it.eu>
2016-12-30 22:14:07 +0100

    Refactor admin bar: - Hide for guests and normal user - Show only if user has edit_posts or upload_files capability - Add filter so other plugins can add/remove capabilities

--- src/plugin_management/class-fum-initialisation.php
+++ src/plugin_management/class-fum-initialisation.php
@@ -7,6 +7,8 @@
 class Fum_Initialisation
 {
 
+    const ADMIN_BAR_CAP_FILTER = Fum_Conf::FUM_NAME_PREFIX . 'admin_bar_cap_filter';
+
     public static function initiate_plugin()
     {
         //Removes 'next' link in head because this could cause SEO problems and firefox is fetching the link in background which causes more traffic
@@ -14,14 +16,13 @@ class Fum_Initialisation
 
         self::add_action_hooks();
         self::add_filter_hooks();
+
         add_shortcode(Fum_Conf::$fum_register_login_page_name, array('Fum_Register_Login_Form_Controller', 'create_register_login_form'));
         add_shortcode(Fum_Conf::$fum_edit_page_name, array('Fum_Edit_Form_Controller', 'create_edit_form'));
         add_shortcode(Fum_Conf::$fum_event_registration_page, array('Fum_Event_Registration_Controller', 'create_event_registration_form'));
         add_shortcode('ems_eventverwaltung', array('Fum_Registered_Event_list', 'create_applied_event_form'));
         add_shortcode('recent_posts', array('Fum_Initialisation', 'my_recent_posts_shortcode'));
         add_shortcode('contact_form', array('Fum_Contact_Form_Controller', 'create_contact_form'));
-
-
     }
 
     public static function my_recent_posts_shortcode($atts)
@@ -44,24 +45,20 @@ class Fum_Initialisation
     }
 
 
-    private static function add_action_hooks()
+    protected static function add_action_hooks()
     {
 
         //Action hook for changing
         add_action('get_header', array('Fum_Initialisation', 'check_shortcode'));
-//Register plugin settings
+        //Register plugin settings
         add_action('admin_init', array('Fum_Option_Page_Controller', 'register_settings'));
-//Create plugin admin menu page
+        //Create plugin admin menu page
         add_action('admin_menu', array('Fum_Option_Page_Controller', 'create_menu'));
 
-
-        /*			add_action( 'wp_before_admin_bar_render', array( 'Admin_Bar', 'create_admin_bar' ) );
-            add_action( 'init', array( 'Admin_Bar', 'show_admin_bar' ) );*/
         add_action('init', array('Fum_Post', 'fum_register_post_type'));
         add_action('init', array(new Fum_Front_End_Form(), 'buffer_content_if_front_end_form'));
 
-
-        /*class-activation-email.php*/
+        add_action('plugins_loaded', array('Fum_Initialisation', 'hideAdminBar'));
 
         //Create activation code on user_register and add it to the user meta
         add_action('user_register', array('Fum_Activation_Email', 'new_user_registered'));
@@ -74,12 +71,12 @@ class Fum_Initialisation
             add_filter('wp_authenticate_user', array('Fum_Activation_Email', 'authenticate'), 10, 1);
         }
 
-//Delete not activated users, if the home url changes, because the activation link may returns a 404 then
+        //Delete not activated users, if the home url changes, because the activation link may returns a 404 then
         add_action('update_option_home', array('Fum_Activation_Email', 'delete_not_activated_users'));
         add_action('update_option_siteurl', array('Fum_Activation_Email', 'delete_not_activated_users'));
 
 
-//Redirect wp-admin/profile.php (Only redirect if the user edits his OWN profile!)
+        //Redirect wp-admin/profile.php (Only redirect if the user edits his OWN profile!)
         add_action('show_user_profile', array('Fum_Redirect', 'redirect_own_profile_edit'));
 
         if (get_option(Fum_Conf::$fum_general_option_group_hide_wp_login_php)) {
@@ -90,18 +87,43 @@ class Fum_Initialisation
         if (get_option(Fum_Conf::$fum_general_option_group_hide_dashboard_from_non_admin)) {
             add_action('wp_dashboard_setup', array('Fum_Redirect', 'redirect_to_home_if_user_cannot_manage_options'));
         }
-
-
     }
 
     private static function add_filter_hooks()
     {
         add_filter('force_ssl', array(new Fum_Front_End_Form(), 'use_ssl_on_front_end_form'), 1, 3);
-
         add_filter('logout_url', array('Fum_Redirect', 'redirect_wp_logout'), 10, 2);
-
     }
 
+    /**
+     * @param null $neededCapabilities
+     * @return bool
+     */
+    public static function hideAdminBar($neededCapabilities = null)
+    {
+        // Admin bar is always hidden for guests, do nothing
+        if (!is_user_logged_in()) {
+            return true;
+        }
+
+        if (empty($neededCapabilities)) {
+            // Admin bar is shown if user has at least one of these capabilities
+            $neededCapabilities = ['upload_files', 'edit_posts'];
+        }
+        $neededCapabilities = apply_filters(static::ADMIN_BAR_CAP_FILTER, $neededCapabilities);
+
+        if (is_array($neededCapabilities)) {
+            foreach ($neededCapabilities as $capability) {
+                if (current_user_can($capability)) {
+                    show_admin_bar(true);
+                    return false;
+                }
+            }
+        }
+
+        show_admin_bar(false);
+        return true;
+    }
 
     /**
      * Calls shortcode callback_header function in wp_head, useful for add styles,scripts, change title, etc.
